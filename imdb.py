49113e3bd4eac55643ea07248d0ebc2fde0834fa
rgurylev <rgurylev@gmail.com>
2024-12-09 14:16:27 +0300

    #1

--- imdb.py
+++ imdb.py
@@ -6,22 +6,11 @@ from torch.utils.data.dataset import random_split
 from ds import ListDataset
 
 
-device = 'cpu'
-def collate_batch(batch):
-    label_list, text_list, lengths = [], [], []
-    for _label, _text in batch:
-        label_list.append(_label)
-        processed_text = torch.tensor(_text, dtype=torch.int64)
-        text_list.append(processed_text)
-        lengths.append(processed_text.size(0))
-    label_list = torch.tensor(label_list)
-    lengths = torch.tensor(lengths)
-    padded_text_list = nn.utils.rnn.pad_sequence(
-        text_list, batch_first=True)
-    return padded_text_list.to(device), label_list.to(device), lengths.to(device)
+
 
 class IMDB():
-    def __init__(self):
+    def __init__(self, device = 'cpu'):
+        self.device = device
         self.vocab = None
         self.x_train = None
         self.y_train = None
@@ -41,11 +30,22 @@ class IMDB():
         self.vocab[1] = "[START]"
         self.vocab[2] = "[OOV]"
 
-    def loader(self, batch_size = 4):
-        DataLoader(self.train_ds, batch_size=batch_size, shuffle=False, collate_fn=collate_batch)
+    def collate_batch(self, batch):
+        label_list, text_list, lengths = [], [], []
+        for _label, _text in batch:
+            label_list.append(_label)
+            processed_text = torch.tensor(_text, dtype=torch.int64)
+            text_list.append(processed_text)
+            lengths.append(processed_text.size(0))
+        label_list = torch.tensor(label_list)
+        lengths = torch.tensor(lengths)
+        padded_text_list = nn.utils.rnn.pad_sequence(
+            text_list, batch_first=True)
+        return padded_text_list.to(self.device), label_list.to(self.device), lengths.to(self.device)
 
     def loader(self, batch_size = 4):
-        return DataLoader(self.train_ds, batch_size=batch_size, shuffle=False, collate_fn=collate_batch)
+        return DataLoader(self.train_ds, batch_size=batch_size, shuffle=False, collate_fn=self.collate_batch)
+
 
     def val_loader(self, batch_size = 4):
-        return DataLoader(self.val_ds, batch_size=batch_size, shuffle=False, collate_fn=collate_batch)
+        return DataLoader(self.val_ds, batch_size=batch_size, shuffle=False, collate_fn=self.collate_batch)
